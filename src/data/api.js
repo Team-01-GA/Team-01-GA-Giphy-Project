9d4b6a586ef22c1d745f8acb80c1dd7b2d160089
Genko Valkinov <117573753+ValkinovG@users.noreply.github.com>
2025-04-15 23:55:12 +0300

    Setup ESLint

--- src/data/api.js
+++ src/data/api.js
@@ -1,12 +1,14 @@
-export function API_KEY(number) {
+export const API_KEY = (number) => {
     switch (number) {
-        case 1:
-            return 'KHzdj1OxPJRiUyRNnS0YUGxGIkLQb8AL';
+    case 1:
+        return 'KHzdj1OxPJRiUyRNnS0YUGxGIkLQb8AL';
 
-        case 2: 
-            return '5l5FeU1u8XiTD86mLuATtsXevbARXXKc';
+    case 2:
+        return '5l5FeU1u8XiTD86mLuATtsXevbARXXKc';
     }
-}
+
+    return -1;
+};
 
 const TRENDING_ENDPOINT = 'https://api.giphy.com/v1/gifs/trending';
 const SEARCH_ENDPOINT = 'https://api.giphy.com/v1/gifs/search';
