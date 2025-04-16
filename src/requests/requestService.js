7bd5387213a9809f3097a0d7a293430088f4616a
Genko Valkinov <117573753+ValkinovG@users.noreply.github.com>
2025-04-16 21:33:31 +0300

    implemented getGifById inside requestService.js

--- src/requests/requestService.js
+++ src/requests/requestService.js
@@ -1,8 +1,21 @@
-import { KEY_1, trendingEndpoint } from '../data/api.js';
+import { KEY_1, KEY_2, trendingEndpoint, singleGifEndpoint } from '../data/api.js';
 
 
 export const getTrendingGifs = async () => {
     const response = await fetch(trendingEndpoint(KEY_1));
 
     return response.json();
+};
+
+
+export const getGifById = async (id) => {
+    try {
+        const response = await fetch(singleGifEndpoint(KEY_2, id));
+        if (!response.ok) throw new Error('Failed to fetch GIF by ID');
+        const data = await response.json();
+        return data.data;
+    } catch (error) {
+        console.error('Error in getGifById:', error);
+        return null;
+    }
 };
