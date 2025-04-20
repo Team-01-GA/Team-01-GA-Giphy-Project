7f297b14859a8a068b0d716a2b52dfe5f0c7ea46
Genko Valkinov <117573753+ValkinovG@users.noreply.github.com>
2025-04-20 16:19:58 +0300

    Added documentation for listView.js

--- src/views/listView.js
+++ src/views/listView.js
@@ -1,6 +1,14 @@
 import { EMPTY_HEART, FULL_HEART } from '../common/constants.js';
 import { getFavoriteIds } from '../events/localStorage.js';
 
+/**
+ * Generates the entire GIF list view section.
+ * Includes title and mapped single GIF cards.
+ *
+ * @param {Object[]} gifList - Array of GIF objects from the API.
+ * @param {string} title - Title to display above the list.
+ * @return {string} - HTML markup for the list view.
+ */
 export const gifListView = (gifList, title) => `
     <div id="gif-list">
         <h1 id="title">${title}</h1>
@@ -10,6 +18,13 @@ export const gifListView = (gifList, title) => `
     </div>
 `;
 
+/**
+ * Generates a single GIF card used in the list view.
+ * Includes a background image, favorite heart button, and title (if available).
+ *
+ * @param {Object} gifObject - The GIF data object.
+ * @return {string} - HTML markup for one GIF card.
+ */
 export const gifSingleView = (gifObject) => {
     const isFav = getFavoriteIds().includes(gifObject.id);
     const heartSymbol = isFav ? FULL_HEART : EMPTY_HEART;
