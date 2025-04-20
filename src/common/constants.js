aaa299520129b2f82a86ec76d3b1ecb8c2a0dc50
Genko Valkinov <117573753+ValkinovG@users.noreply.github.com>
2025-04-20 16:05:14 +0300

    Added documentation for constants.js

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,27 +1,23 @@
+// Page view identifiers
 export const TRENDING = 'TRENDING';
-
 export const SEARCH = 'SEARCH';
-
 export const DETAILS = 'DETAILS';
-
 export const UPLOAD = 'UPLOAD';
-
 export const FAVORITES = 'FAVORITES';
 
+// Number of GIFs to fetch (as string for use in URL params)
 export const GIF_LIMIT = '50';
 
+// Symbols used for favoriting UI
 export const FULL_HEART = '❤';
-
 export const EMPTY_HEART = '♡';
 
+// Local storage keys
 export const FAVORITES_KEY = 'favorites';
-
 export const UPLOADS_KEY = 'uploads';
 
+// Titles used in each view section
 export const TRENDING_TITLE = 'Trending GIFs right now';
-
 export const SEARCH_TITLE = 'Results for ';
-
 export const FAVORITES_TITLE = 'Your favorite GIFs';
-
 export const UPLOADED_TITLE = 'Your uploaded GIFs';
